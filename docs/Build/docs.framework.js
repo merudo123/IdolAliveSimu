e789fd7bf6f9ee0452d14b257086f3117ccde955
merudo123 <ungekino-sub@yahoo.co.jp>
2025-01-11 19:22:40 +0900

    250111

--- docs/Build/docs.framework.js
+++ docs/Build/docs.framework.js
@@ -2078,13 +2078,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  4544544: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 4544605: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 4544669: function() {return Module.webglContextAttributes.powerPreference;},  
- 4544727: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 4544782: function($0) {performance.now = function() { return $0; };},  
- 4544830: function($0) {performance.now = function() { return $0; };},  
- 4544878: function() {performance.now = Module['emscripten_get_now_backup'];}
+  4544640: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 4544701: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 4544765: function() {return Module.webglContextAttributes.powerPreference;},  
+ 4544823: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 4544878: function($0) {performance.now = function() { return $0; };},  
+ 4544926: function($0) {performance.now = function() { return $0; };},  
+ 4544974: function() {performance.now = Module['emscripten_get_now_backup'];}
 };
 
 
